51cb32cdef4095e352e8afdaa0f09acff3087ef1
ullayne fernandes <uffl@cin.ufpe.br>
2017-06-06 20:01:27 +0000

    cenarios de controlador

---
+++ features/step_definitions/validacao_de_facilitador_steps.rb
@@ -0,0 +1,93 @@
+Given(/^que o facilitador de login "([^"]*)" não está associado a nenhum laboratório$/) do |fac_name|
+    
+    param_fac = {user: {name: fac_name, email: "uffl@cin.ufpe.br", password: "adc", kind: "fac"}}
+    post '/users', param_fac
+    p param_fac
+    p fac_name
+    fac = User.find_by(name: fac_name)
+    p fac
+    expect(fac).to_not be nil
+    
+end
+
+When(/^o facilitador "([^"]*)" faz uma requisicao de acesso para o laboratorio "([^"]*)"$/) do |fac_name, lab_name|
+    
+    param_lab = {laboratory: {name: lab_name, department: nil}}
+    post '/laboratories', param_lab
+    p_lab = Laboratory.find_by(name: lab_name)
+    expect(p_lab).to_not be nil
+    
+end
+
+When(/^o sistema verifica se o facilitador "([^"]*)" está associado a um laboratório\.$/) do |fac_name|
+    
+    param_fac = {user: {name: fac_name, email: "fac@hotmail.com", password: "aa", kind: "fac"}}
+    post '/users', param_fac
+    p_user = User.find_by(name: fac_name)
+    expect(p_user).to_not be nil
+    p_lab = Laboratory.find_by(user_id: p_user.id)
+    expect(p_lab).to_not be !nil
+
+end
+
+Then(/^o sistema faz a requisicao do facilitador "([^"]*)" ao laboratorio "([^"]*)"$/) do |fac_name, lab_name|
+    
+    p_user = User.find_by(name: fac_name)
+    p_lab = Laboratory.find_by(name: lab_name)
+    expect(p_user).to_not be nil
+    expect(p_lab).to_not be nil
+    param_req = {request: {user_id: p_user.id, laboratory_id: p_lab.id}}
+    post '/requests', param_req
+    p_req = Request.find_by(user_id: p_user.id)
+    p p_req
+    
+end
+
+Given(/^o adm "([^"]*)" esta asscoiado ao sistema$/) do |adm_name|
+    param_adm = {user: {name: adm_name, email: "adm_name@ha.com", password: "blabla", kind: "adm"}}
+    post '/users', param_adm
+    p param_adm
+    adm = User.find_by(name: adm_name)
+    p param_adm
+    expect(adm).to_not be nil
+end
+
+Then(/^o sistema faz uma notificacao avisando que o facilitador "([^"]*)" fez uma requisicao para o laboratorio "([^"]*)"$/) do |fac_name, lab_name|
+    
+    fac = User.find_by(name: fac_name)
+    expect(fac).to_not be nil
+    
+    lab = Laboratory.find_by(name: lab_name)
+    expect(lab).to_not be nil
+    p fac.id
+    req = Request.find_by(user_id: fac.id)
+    expect(req).to_not be nil
+
+    param_not = {notification: {message: "Nova requisicao",request_id: req.id}}
+    post '/notifications', param_not
+    noti = Notification.find_by(request_id: req.id)
+    expect(noti).to_not be nil
+
+end
+
+Then(/^o sistema envia um email para "([^"]*)" avisando que existe uma nova requisicao$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^eu estou logado como adm$/) do
+   # expect(get("/")).to route_to("main_adm")
+    expect(:get => "/main_adm").to route_to(:controller => "user")
+
+end
+
+When(/^eu entro na pagina "([^"]*)"$/) do |page|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^eu aceito as solicitacoes pendentes$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^eu aceito as solicitações pendentes$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/validacao_de_facilitador.feature
+++ features/validacao_de_facilitador.feature
@@ -1,9 +1,39 @@
-#Feature: Validação de Facilitador
-#  As a usuário Administrador
-#  I want to validar o acesso de um Facilitador ao sistema ligado a um dado Departamento
-#  So that posso conectar um Facilitador a um Departamento e liberar o acesso dele ao sistema
-#  
-#  @d1
-#  @d2
-#  @d3
-#  @d4
+Feature: Validação de Facilitador
+  As a usuário Administrador
+  I want to validar o acesso de um Facilitador ao sistema ligado a um dado Departamento
+  So that posso conectar um Facilitador a um Departamento e liberar o acesso dele ao sistema
+ 
+  @d1
+ Scenario: O facilitador solicita  acesso ao laboratório e o facilitador não está vinculado a nenhum laboratório
+    Given que o facilitador de login "joc" não está associado a nenhum laboratório
+    When o facilitador "joc" faz uma requisicao de acesso para o laboratorio "quimica"
+    And o sistema verifica se o facilitador "joc" está associado a um laboratório.
+    Then o sistema faz a requisicao do facilitador "joc" ao laboratorio "quimica"
+
+  @d2
+  Scenario: O administrador recebe um email de aviso de nova requisição
+    Given o adm "joc" esta asscoiado ao sistema
+    When o facilitador "miriane" faz uma requisicao de acesso para o laboratorio "quimica"
+    And o sistema verifica se o facilitador "miriane" está associado a um laboratório.    
+    And o sistema faz a requisicao do facilitador "miriane" ao laboratorio "quimica"
+    Then o sistema faz uma notificacao avisando que o facilitador "miriane" fez uma requisicao para o laboratorio "quimica"
+
+  @d3
+  Scenario: Administrador tem solicitações de acesso ao laboratório pendentes
+    Given eu estou logado como adm
+    When eu entro na pagina "request"
+    Then eu aceito as solicitacoes pendentes
+
+  @d4
+  Scenario: O facilitador já associado a um laboratório  solo e pede acesso a outro laboratório
+    Given o facilitador "lar" esta associado ao laboratorio de "fisica"  
+    When eu faço uma requisição de acesso para o laboratório "química"
+    Then eu vejo uma mensagem informando que o facilitador "lar" ja esta associado a um laboratório
+
+
+  @d5
+  Scenario: O administrador aceita ou rejeita uma requisição a um laboratório
+    Given estou logado como "admin"
+    When eu estou na "página de solicitações"
+    Then eu aceito ou rejeito as solicitações pendentes dos facilitadores
+
